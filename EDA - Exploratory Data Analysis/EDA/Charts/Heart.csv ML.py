79c1bce5dafd971439693205557949a4c22f0b1b
Wesołowski <jakub.a.wesolowski@capgemini.com>
2026-06-08 08:03:06 +0200

    Initial commit

--- EDA - Exploratory Data Analysis/EDA/Charts/Heart.csv ML.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-df = pd.read_csv('heart.csv')
-print(df)
-
-print('\nDescribe:')
-prin(df.describe().T.round(2).to_string())
--- EDA - Exploratory Data Analysis/EDA/Charts/sciąga.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-# Wczytywane danych pliku
-df
